e01662b47aebd6a111f7d80f6fbff2d2f8fa4b47
Xun Yang <xun.yang@1337.tech>
2022-10-24 22:05:54 +0200

    Add basic structure for Mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,28 @@
-import {QueryClient, QueryClientProvider} from "react-query";
-import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 import SortContextProvider from "../contexts/SortContext/SortContextProvider";
 import FilterContextProvider from "../contexts/FilterContext/FilterContextProvider";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <QueryClientProvider client={queryClient}>
-    <SortContextProvider>
-    <FilterContextProvider>
-      <Component {...pageProps} />
-    </FilterContextProvider>
-    </SortContextProvider>
-  </QueryClientProvider>
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SortContextProvider>
+          <FilterContextProvider>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+              <Component {...pageProps} />
+            </MantineProvider>
+          </FilterContextProvider>
+        </SortContextProvider>
+      </QueryClientProvider>
+    </>
+  );
 }
 
-
-export default MyApp
+export default MyApp;
