8a58ea3b2bb3f94898a56d9121087e848ad9106d
Xun Yang <xun.yang@1337.tech>
2022-10-24 14:40:50 +0200

    Remove more errors

--- lib/frontendApi.ts
+++ lib/frontendApi.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 import {Coworker} from "../interfaces/CoworkerModel";
 
-import mockData from '../mocks/1337api-v3-employees.json';
-
 type CoworkersApiType = () => Promise<Coworker[]>
 export const coworkersApi:CoworkersApiType = async () => {
   const {data} = await axios.get('/api/getCoworkers');
