d5baead2831151b18a5c6a4f9409875b88aa3365
Xun Yang <xun.yang@1337.tech>
2022-10-27 17:16:55 +0200

    Use framer-motion for MemoryStage

--- components/CoworkersList/CoworkersList.tsx
+++ components/CoworkersList/CoworkersList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Transition } from "@mantine/core";
+import { motion } from "framer-motion";
 
 import { Coworker as CoworkerModel } from "../../interfaces/CoworkerModel";
 
@@ -7,49 +7,44 @@ import Coworker from "../Coworker/Coworker";
 
 import styles from "./CoworkersList.module.scss";
 
+const ulVariants = {
+  open: {
+    transition: { staggerChildren: 0.07 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const liVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
 interface Props {
   coworkers: CoworkerModel[];
 }
-
-const slideUp = {
-  in: { opacity: 1, transform: "translateY(0)" },
-  out: { opacity: 0, transform: "translateY(20px)" },
-  common: { transformOrigin: "top" },
-  transitionProperty: "transform, opacity",
-};
-
 const CoworkersList: React.FC<Props> = ({ coworkers }) => {
-  const [mounted, setMounted] = useState(-1);
-  useEffect(() => {
-    setMounted(0);
-    return () => {
-      setMounted(-1);
-    };
-  }, []);
   return (
-    <ul className={styles.CoworkersList}>
+    <motion.ul className={styles.CoworkersList} variants={ulVariants}>
       {coworkers.map((coworker, idx) => (
-        <li key={idx}>
-          <Transition
-            mounted={mounted >= idx}
-            transition={slideUp}
-            duration={2000}
-            timingFunction="cubic-bezier(0.230, 1.000, 0.320, 1.000)"
-            onEnter={() => {
-              setTimeout(() => {
-                setMounted(idx + 1);
-              }, 140);
-            }}
-          >
-            {(styles) => (
-              <div style={styles}>
-                <Coworker {...coworker} />
-              </div>
-            )}
-          </Transition>
-        </li>
+        <motion.li key={idx} variants={liVariants}>
+          <Coworker {...coworker} />
+        </motion.li>
       ))}
-    </ul>
+    </motion.ul>
   );
 };
 export default CoworkersList;
