de31e31f63dd4e051aa2110f53d4bf14b3577371
Xun Yang <xun.yang@1337.tech>
2022-10-27 13:06:38 +0200

    Improve FlagText shadow, remove confetti after result is shown, speed up background animation

--- containers/ResultStage/ResultStage.tsx
+++ containers/ResultStage/ResultStage.tsx
@@ -100,6 +100,14 @@ const ResultStage: React.FC<ResultStageProps> = ({
     };
   }, [testStarter]);
 
+  useEffect(() => {
+    if (stage >= STAGES.RESULT_DISPLAY) {
+      setTimeout(() => {
+        clearTimeout(confettiTimerRef.current);
+      }, 500);
+    }
+  }, [stage]);
+
   const onGameRestartClick = () => {
     gameDispatch({ type: GAME_ACTIONS.RESTART });
   };
