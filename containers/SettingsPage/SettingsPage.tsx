15e80eac74748963934c46e75819cd3ddc6de438
Xun Yang <xun.yang@1337.tech>
2023-01-24 13:22:31 +0100

    UseReducer instead. Add a placeholder option

--- containers/SettingsPage/SettingsPage.tsx
+++ containers/SettingsPage/SettingsPage.tsx
@@ -2,7 +2,7 @@ import { Stack, Checkbox, CheckboxProps } from "@mantine/core";
 
 import BottomBar from "../../components/BottomBar/BottomBar";
 import TitleText from "../../components/TitleText/TitleText";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { initGameDB } from "../../lib/gameDB";
 
 const SettingCheckbox = (props: CheckboxProps) => {
@@ -10,25 +10,55 @@ const SettingCheckbox = (props: CheckboxProps) => {
     <Checkbox size="lg" styles={{ label: { color: "white" } }} {...props} />
   );
 };
+
+enum GAME_SETTINGS {
+  TYPE_NAMES = "typeNames",
+  REVEAL_OPTIONS = "revealOptions",
+}
+
+const initialSettings = {
+  [GAME_SETTINGS.TYPE_NAMES]: false,
+  [GAME_SETTINGS.REVEAL_OPTIONS]: false,
+};
+
+interface SettingsReducerAction {
+  type: GAME_SETTINGS;
+  value: boolean;
+}
+
+const reducer = (
+  state: typeof initialSettings,
+  action: SettingsReducerAction
+) => {
+  return {
+    ...state,
+    [action.type]: action.value,
+  };
+};
+
 export const SettingsPage = () => {
   const dbRef = useRef<Awaited<ReturnType<typeof initGameDB>>>();
-  const [typeNames, setTypeNames] = useState<boolean>(false);
+
+  const [state, dispatch] = useReducer(reducer, initialSettings);
 
   useEffect(() => {
     (async () => {
       const db = await initGameDB();
       dbRef.current = db;
-      const dbTypeName = (await db.getSetting("typeNames")) === "true";
-      setTypeNames(dbTypeName);
+      for (let stateKey in state) {
+        const dbValue = (await db.getSetting(stateKey)) === "true";
+        dispatch({ type: stateKey as GAME_SETTINGS, value: dbValue });
+      }
     })();
   }, []);
 
-  const typeNamesChanged = () => {
-    const newVal = !typeNames;
-    setTypeNames(newVal);
+  const checkBoxChanged = (key: GAME_SETTINGS) => {
+    const newVal = !state[key];
+    dispatch({ type: key, value: newVal });
     if (!dbRef.current) throw "dbRef is not set yet";
-    dbRef.current.saveSetting("typeNames", "" + newVal);
+    dbRef.current.saveSetting(key, "" + newVal);
   };
+
   return (
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
@@ -36,10 +66,15 @@ export const SettingsPage = () => {
       <Stack style={{ flexGrow: 1, marginLeft: "2rem" }}>
         <TitleText align={"left"}>Settings</TitleText>
         <SettingCheckbox
-          checked={typeNames}
-          name={"typeNames"}
-          label={"Type out the names"}
-          onChange={typeNamesChanged}
+          checked={state[GAME_SETTINGS.TYPE_NAMES]}
+          label={"During play, names must be typed out"}
+          onChange={() => checkBoxChanged(GAME_SETTINGS.TYPE_NAMES)}
+        />
+        <SettingCheckbox
+          disabled={true}
+          checked={state[GAME_SETTINGS.REVEAL_OPTIONS]}
+          label={"During play, options will be revealed after a click"}
+          onChange={() => checkBoxChanged(GAME_SETTINGS.REVEAL_OPTIONS)}
         />
       </Stack>
       <BottomBar hasBack />
