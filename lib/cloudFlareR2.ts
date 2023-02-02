d65449d9d65e910e0ce3c29133a1b495adeaac09
Xun Yang <xun.yang@1337.tech>
2023-02-02 13:26:43 +0100

    Fix typescript issue

--- lib/cloudFlareR2.ts
+++ lib/cloudFlareR2.ts
@@ -17,8 +17,8 @@ const S3 = new S3Client({
   region: "auto",
   endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY,
+    accessKeyId: ACCESS_KEY_ID || "",
+    secretAccessKey: SECRET_ACCESS_KEY || "",
   },
 });
 
